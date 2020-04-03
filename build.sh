2b48e55e3ff10b90d792bbdcf09bd4b357905b2e
shouldsee <shouldsee@github.com>
2020-04-03 17:14:46 +0800

    standalone html

--- build.sh
+++ build.sh
@@ -3,10 +3,17 @@ DIR=$PWD
 
 mkdir -p _build/
 cd _build
-curl -LC- -o pandoc.css https://gist.github.com/killercup/5917178/raw/40840de5352083adb2693dc742e9f75dbb18650f/pandoc.css
-# pandoc --standalone --css https://gist.githubusercontent.com/killercup/5917178/raw/40840de5352083adb2693dc742e9f75dbb18650f/pandoc.css --from gfm --to html README.md >index.html
-pandoc --standalone --css pandoc.css --from gfm --to html $DIR/README.md >$DIR/index.html
+## ref: https://devilgate.org/blog/2012/07/02/tip-using-pandoc-to-create-truly-standalone-html-files/#fnref1
+## pandoc -s -S --toc -H pandoc.css -A footer.html README -o example3.html
+curl -LC- -o pandoc.css.temp https://gist.github.com/killercup/5917178/raw/40840de5352083adb2693dc742e9f75dbb18650f/pandoc.css
+{
+	echo '<style type="text/css">' 
+	cat pandoc.css.temp 
+	echo '</style>'
+} > pandoc.css
 
+pandoc --standalone --include-in-header pandoc.css --from gfm --to html README.md >$DIR/index.html
 head $DIR/index.html
+
 cd $DIR
 echo [build_succ]
